19a07b549d084ca93e46d4a21480cc1138c5695a
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-04-22 17:53:12 -0300

    Processo está lento

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
-function alterarStatus(id){
+function alterarStatus(id) {
     let gameEscolido = document.getElementById(`game-${id}`);
     let imgGame = gameEscolido.querySelector('.dashboard__item__img');
     let nomeGame = gameEscolido.querySelector('.dashboard__item__name');
+    let botao = gameEscolido.querySelector('.dashboard__item__button');
 
-    alert(nomeGame.textContent);
 
 
+    if (botao.classList.contains('dashboard__item__button')) {
+        botao.classList.add('dashboard__item__button--return');
+    } else if (botao.classList.contains('dashboard__item__button--return')) {
+        botao.classList.remove('dashboard__item__button--return');
+    }
 
 
 }
