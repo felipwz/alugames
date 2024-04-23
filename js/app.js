23e4443239d9b54ca26490f62900b56922db9237
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2024-04-23 17:38:03 -0300

    Adicionando funcionalidades extras
    Adicionado a função "confirmacaoDeDevolucao(imgGame, botao)", que verifica se o usuário deseja mesmo devolver o game.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
-// Finalizado
+function confirmacaoDeDevolucao(imgGame, botao) {
+    alert("Você deseja devolver esse jogo?");
+    var resposta = confirm("Clique em OK para 'Sim' ou em Cancelar para 'Não'.");
+
+    if (resposta) {
+        imgGame.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.textContent = 'Alugar';
+
+    } else {
+
+
+    }
+}
+
+
 function alterarStatus(id) {
     let gameEscolido = document.getElementById(`game-${id}`);
     let imgGame = gameEscolido.querySelector('.dashboard__item__img');
@@ -8,18 +23,16 @@ function alterarStatus(id) {
 
 
     if (imgGame.classList.contains('dashboard__item__img--rented')) {
-        imgGame.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
-        
+        confirmacaoDeDevolucao(imgGame, botao);
 
     } else {
         imgGame.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
-        
-        
+
+
     }
 
 
 }
+
